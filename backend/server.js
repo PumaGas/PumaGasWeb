ff5a1a0c545d6a0120baa0da58bf3ef606b5a1ea
Rai Umer <raifarooq7860786@gmail.com>
2025-03-11 02:27:51 +0500

    Completed code 28

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,18 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 }
 
 // Connect db
-//connectDatabase();
+connectDatabase();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", frontendUrl);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true"); 
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,

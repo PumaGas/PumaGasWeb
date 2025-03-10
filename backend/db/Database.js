ab72c610da414501ad862c5cf930326238010e02
Rai Umer <raifarooq7860786@gmail.com>
2025-03-10 21:18:27 +0500

    Completed code 16

--- backend/db/Database.js
+++ backend/db/Database.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose");
 
-const connectDatabase = () => {
-  mongoose
-    .connect(process.env.DB_URL, {
+const connectDatabase = async () => {
+  try {
+    const connection = await mongoose.connect(process.env.DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    })
-    .then((data) => {
-      console.log(`mongod connected with server: ${data.connection.host}`);
     });
+    console.log(`MongoDB connected: ${connection.connection.host}`);
+  } catch (error) {
+    console.error(`MongoDB connection error: ${error.message}`);
+    process.exit(1); // Exit process with failure
+  }
 };
 
 module.exports = connectDatabase;
